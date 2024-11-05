1255a50cbee3bf57cea732a9732f026735fb7f01
rxElectron <kh.reza10@gmail.com>
2024-11-05 06:56:44 +0330

    debug conflict of blueprint ai_ml_tools same name functions in project ...

--- app/ai_ml_tools/routes.py
+++ app/ai_ml_tools/routes.py
@@ -1,6 +1,9 @@
 from . import ai_ml_tools
 from flask import render_template
 
-@ai_ml_tools.route('/')
-def index():
+@ai_ml_tools.route('/ai_ml_tools')
+def show_ai_ml_tools():
     return render_template('ai_ml_tools.html')
+
+# def ai_ml_tools():
+#     return render_template('ai_ml_tools.html')
